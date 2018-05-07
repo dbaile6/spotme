c9dac822d9d0990e0c1aa904e158c7de0e12b996
Dylan Bailey <dylanba1@gmail.com>
2018-05-07 14:45:16 -0400

    Integrate user authentication with API key

--- src/Components/UserAuth.js
+++ src/Components/UserAuth.js
@@ -1,6 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import Config from '../public/developer.json'
 
-export const authURL = (
-    <a href="https://accounts.spotify.com/authorize">Spotify Login</a>
-)
+export default class Auth extends React.Component {
+    render() {
+        const hostUrl = 'https://accounts.spotify.com'
+        const urlRedirect = `${window.location.protocol}//${window.location.hostUrl}/auth`
+        const scopes = 'user-library-read'
+        const authUrl = `${hostUrl}/authorize?response_type=token` +
+            `&redirect_uri=${encodeURIComponent(urlRedirect)}` +
+            `&client_id=${Config.spotify.clientId}` +
+            `&scope=${encodeURIComponent(scopes)}`
+        return ( <a href = {authUrl} > Sign into Spotify </a>
+        )
+    }
+}

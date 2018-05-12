3869a035594d400cceee0369ccb57278adbc8c49
Dylan Bailey <dylanba1@gmail.com>
2018-05-12 02:42:01 -0400

    Fixed Authentication

--- src/Components/UserAuth.js
+++ src/Components/UserAuth.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import Config from '../public/developer.json'
 
-export class Auth extends React.Component {
-    render() {
-        const hostUrl = 'https://accounts.spotify.com'
-        const urlRedirect = `${window.location.protocol}//${window.location.hostUrl}/auth`
-        const scopes = 'user-library-read'
-        const authUrl = `${hostUrl}/authorize?response_type=token` +
-            `&redirect_uri=${encodeURIComponent(urlRedirect)}` +
-            `&client_id=${Config.spotify.clientId}` +
-            `&scope=${encodeURIComponent(scopes)}`
-        return ( <a href = {authUrl} > Sign into Spotify </a>
-        )
-    }
+import Config from '../public/config'
+
+export default class Auth extends React.Component {
+  render() {
+    const SpotLogIn = 'https://accounts.spotify.com'
+    const redirectUri = `localhost`
+    const scopes = 'user-library-read playlist-modify-public'
+    const authUrl = `${SpotLogIn}/authorize?response_type=token` +
+      `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+      `&client_id=${Config.spotify.clientId}` +
+      `&scope=${encodeURIComponent(scopes)}`
+    return (
+      <section className="hero">
+        <div className="hero-body">
+          <div className="container">
+            <h2 className="subtitle">
+              Sign in with your Spotify account to see trends about your saved songs.
+            </h2>
+            <p>
+              <a
+                href={authUrl}
+                className="spotify-button is-primary button is-large"
+              >Sign into Spotify</a>
+            </p>
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
